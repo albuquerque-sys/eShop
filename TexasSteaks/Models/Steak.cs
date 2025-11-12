056c221ea05ff545a46e97ce703fcaff3bc1c213
Bernardo Marambaia <marambaia.bma@gmail.com>
2025-11-12 20:42:25 -0300

    Create the domain model.

---
+++ TexasSteaks/Models/Steak.cs
@@ -0,0 +1,18 @@
+﻿namespace TexasSteaks.Models
+{
+    public class Steak
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string ShortDescription { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public string ImageURL { get; set; }
+        public string ThumbnailURL { get; set; }
+        public bool IsFavorite { get; set; }
+        public bool InStock { get; set; }
+
+        public int CategoryId { get; set; }
+        public virtual Category Category { get; set; }
+    }
+}
