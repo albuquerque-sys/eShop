402cce22d26d37b0f59dbf978814eb3d2b31b95a
Bernardo Marambaia <marambaia.bma@gmail.com>
2025-11-12 22:33:26 -0300

    Create a context class in the project.

---
+++ TexasSteaks/Context/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TexasSteaks.Models;
+
+namespace TexasSteaks.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Steak> Steaks { get; set; }
+    }
+}
