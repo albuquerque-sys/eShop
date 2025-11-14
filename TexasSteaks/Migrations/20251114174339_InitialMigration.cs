eca6f38d12d1222863ef3b75ad268d4072b18b7c
Bernardo Marambaia <marambaia.bma@gmail.com>
2025-11-14 17:30:20 -0300

    Initial migration and populate data.

---
+++ TexasSteaks/Migrations/20251114174339_InitialMigration.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TexasSteaks.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Categories",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Categories", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Steaks",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    ShortDescription = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: false),
+                    Price = table.Column<decimal>(type: "decimal(7,2)", nullable: false),
+                    ImageURL = table.Column<string>(type: "nvarchar(300)", maxLength: 300, nullable: true),
+                    ThumbnailURL = table.Column<string>(type: "nvarchar(300)", maxLength: 300, nullable: true),
+                    IsFavorite = table.Column<bool>(type: "bit", nullable: false),
+                    InStock = table.Column<bool>(type: "bit", nullable: false),
+                    CategoryId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Steaks", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Steaks_Categories_CategoryId",
+                        column: x => x.CategoryId,
+                        principalTable: "Categories",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Steaks_CategoryId",
+                table: "Steaks",
+                column: "CategoryId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Steaks");
+
+            migrationBuilder.DropTable(
+                name: "Categories");
+        }
+    }
+}
---
+++ TexasSteaks/Migrations/20251114182225_PopulateCategories.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TexasSteaks.Migrations
+{
+    /// <inheritdoc />
+    public partial class PopulateCategories : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("INSERT INTO Categories(Name, Description) VALUES ('Churrasco na grelha', 'Versátil e tradicional para a maioria dos cortes.')");
+            migrationBuilder.Sql("INSERT INTO Categories(Name, Description) VALUES ('Churrasco no espeto', 'Usado para cortes grandes como o costelão 12 horas ou picanha, preservando suculência.')");
+            migrationBuilder.Sql("INSERT INTO Categories(Name, Description) VALUES ('Churrasco estilo americano (Barbecue)', 'Caracterizado pela defumação lenta, usando marinadas e temperos.')");
+            migrationBuilder.Sql("INSERT INTO Categories(Name, Description) VALUES ('Parrilla', ' Estilo da culinária argentina e uruguaia, com grelhas baixas e cortes de carne preparados de forma específica.')");
+            migrationBuilder.Sql("INSERT INTO Categories(Name, Description) VALUES ('Delivery/Marmitex', 'Opções práticas para quem pede comida em casa, como o churrasco em marmitas.')");
+            migrationBuilder.Sql("INSERT INTO Categories(Name, Description) VALUES ('Steakhouse', 'Restaurantes especializados em carnes, com cortes nobres e diferentes técnicas de preparo.')");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DELETE FROM Categories");
+        }
+    }
+}
