1ef33ba53d88eb1204940f60bf3b15067ca3c894
Bernardo Marambaia <marambaia.bma@gmail.com>
2025-11-27 02:18:39 -0300

    Creating the ShoppingCartViewModel.

---
+++ TexasSteaks/ViewModels/ShoppingCartViewModel.cs
@@ -0,0 +1,10 @@
+﻿using TexasSteaks.Models;
+
+namespace TexasSteaks.ViewModels
+{
+    public class ShoppingCartViewModel
+    {
+        public ShoppingCart ShoppingCart { get; set; }
+        public decimal ShoppingCartTotal { get; set; }
+    }
+}
