c315d5dbcd0eb85a32617a2f87a2371883b96668
Bernardo Marambaia <marambaia.bma@gmail.com>
2025-12-15 22:59:46 -0300

    Creating the contact area and defining a TagHelper.

---
+++ eShop/Controllers/ContactController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace eShop.Controllers
+{
+    public class ContactController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
